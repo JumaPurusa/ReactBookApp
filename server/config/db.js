d1b038f9403aa97f99ef2d49160ec01fcf4140d8
Juma M Purusa <jmpurusa@gmail.com>
2025-03-24 17:58:58 +0300

    Creating a server with express - improving server startup

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+const db = {
+    'user': 'jumapurusa',
+    'password': 'Jmp1009.'
+};
+
+module.exports = db;
---
+++ server/routes/books.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../config/db');
+
+console.log(db.user, db.password);
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('books here!');
+});
+
+module.exports = router;
